ad688630638c20d31b8dda6e3bc56f4d9dbf58fa
javaprovip <sergiokravchenko@gmail.com>
2022-06-22 17:38:32 +0300

    third oop stream

---
+++ resources/Task3.java
@@ -0,0 +1,32 @@
+package com.lessons.dvd.lesson22.task;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+private class Task3 {
+    private static void main(String[] args) throws IOException {
+        Path path = Path.of("resources", "test.txt");
+        Files.readAllLines(path).stream()
+                .map(Task3::findMaxDigitsCount)
+                .forEach(System.out::println);
+
+    }
+
+    private static Integer findMaxDigitsCount(String line) {
+        Integer result = 0;
+        int counter = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (Character.isDigit(line.charAt(i))){
+                counter++;
+            }else {
+                if(result < counter){
+                    result = counter;
+                }
+                counter = 0;
+            }
+
+        }
+        return result;
+    }
+}
---
+++ resources/Task4.java
@@ -0,0 +1,18 @@
+;ksat.22nossel.dvd.snossel.moc egakcap;noitpecxEOI.oi.avaj tropmi;seliF.elif.oin.avaj tropmi;htaP.elif.oin.avaj tropmi{ 4ksaT ssalc cilbup{ noitpecxEOI sworht )sgra ][gnirtS(niam diov citats cilbup    ;)"avaj.3ksaT" ,"ksat" ,"22nossel" ,"dvd" ,"snossel" ,"moc" ,"crs"(fo.htaP = htap htaP        ;)htap(gnirtSdaer.seliF = eulaVgnirts gnirtS        ;)"etavirp" ,"cilbup"(ecalper.eulaVgnirts = tluser gnirtS        ;)"avaj.3ksaT" ,"secruoser"(fo.htaP = htaPtluser htaP        ;)tluser,htaPtluser(gnirtSetirw.seliF        }    };ksat.22nossel.dvd.snossel.moc egakcap
+
+;noitpecxEOI.oi.avaj tropmi
+;seliF.elif.oin.avaj tropmi
+;htaP.elif.oin.avaj tropmi
+
+{ 4ksaT ssalc cilbup
+{ noitpecxEOI sworht )sgra ][gnirtS(niam diov citats cilbup    
+;)"avaj.3ksaT" ,"ksat" ,"22nossel" ,"dvd" ,"snossel" ,"moc" ,"crs"(fo.htaP = htap htaP        
+;)htap(gnirtSdaer.seliF = eulaVgnirts gnirtS        
+;)"etavirp" ,"cilbup"(ecalper.eulaVgnirts = tluser gnirtS        
+
+;)"avaj.3ksaT" ,"secruoser"(fo.htaP = htaPtluser htaP        
+;)tluser,htaPtluser(gnirtSetirw.seliF        
+
+}    
+
+}
